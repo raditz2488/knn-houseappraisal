68598ed787c4e644528490c07a6e0269d221c280
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2021-07-15 19:54:12 +0530

    Update: 1. Updated the recursionlimit to 10000 2. Created the class Regression and added initializer.

--- Regressor.py
+++ Regressor.py
@@ -7,3 +7,19 @@ from scipy.spatial import KDTree
 from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
 
+# Lets increase the recursionlimit as KDTree throws an error if it cant recurse 
+sys.setrecursionlimit(10000)
+
+class Regression(object):
+    '''
+    This class performs kNN regression
+    '''
+
+    def __init__(self):
+        self.k = 5
+        self.metric = np.mean
+        self.kdtree = None
+        self.houses = None
+        self.prices = None
+
+    self.
